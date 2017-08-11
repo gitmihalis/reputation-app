5121a6f161af386dd45b7a8a350e3c5fa238bb3b
Mihalis Fthenos <mihalisfthenos@icloud.com>
2017-08-11 14:39:07 -0400

    add profile_id to users table

--- db/migrate/20170811173632_create_users.rb
+++ db/migrate/20170811173632_create_users.rb
@@ -1,7 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.integer :profile_id
+      t.string   :first_name
+      t.string   :last_name
+      t.string   :email
+      t.string   :password_digest
+      t.integer   :profile_id, foreign_key: true
       t.timestamps
     end
   end
