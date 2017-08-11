e8194563f247a3a395de7161be18fa2f64358563
lpenstone <laura.penstone@queensu.ca>
2017-08-11 15:45:51 +0000

    Created models to match database

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+  belongs_to  :category
+  belongs_to :author, class_name: 'User'
+  belongs_to :receiver, class_name: 'User'
+  has_one :comment
+  has_many :flags
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :reviews
+  has_many :comments, through: :reviews
+  has_many :flags
+
+  has_one :profile
 end
