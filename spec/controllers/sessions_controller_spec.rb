d376bf3332cac601ef1845cdfe9a1ffbd6045cc7
Mihalis Fthenos <mihalisfthenos@icloud.com>
2017-08-12 18:30:45 -0400

    basic login form

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,10 +2,9 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
 
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
+  describe "login a user", type: :request  do
+    it "should redirect to login_path if credentials are invalid" do
+      get login_path
     end
   end
 
