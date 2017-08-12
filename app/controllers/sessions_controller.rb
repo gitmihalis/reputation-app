cf7798f9394fcd2ab4cd75024f9507a2395f2cf8
Mihalis Fthenos <mihalisfthenos@icloud.com>
2017-08-12 13:08:27 -0400

    add sign in logic to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    # If the user exists AND the password is valid
+    if user && user.authenticate_with_credentials(params[:email], params[:password])
+      # Set the user id in the cookie.
+      sessions[:user_id] = user.id
+      redirect_to '/'
+    else
+      # If user login info is not valid,
+      redirect_to '/login'
+    end
   end
 
   def destroy
+    sessions[:user_id] = nil
+    redirect_to '/login'
   end
   
 end
