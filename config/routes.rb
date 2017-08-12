dbacb9c10bc0a0f780aac5f70f9baca1c8e1e2f5
lpenstone <laura.penstone@queensu.ca>
2017-08-12 16:56:14 +0000

    Added categories pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'categories/new'
+  end
+
   get 'welcome/index'
 
   root 'welcome#index'
@@ -11,6 +15,7 @@ Rails.application.routes.draw do
     root to: 'reviews#index'
     resources :reviews, only: [:index, :destroy]
     resources :users, only: [:index, :destroy]
+    resources :catagories, only: [:index, :new, :create, :destroy]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
