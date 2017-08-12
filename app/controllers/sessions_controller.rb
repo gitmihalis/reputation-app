755afc499a3a993129843b74d4f3355137b84151
Mihalis Fthenos <mihalisfthenos@icloud.com>
2017-08-12 18:40:38 -0400

    make flash message partial

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,8 @@ class SessionsController < ApplicationController
 
   def destroy
     sessions[:user_id] = nil
-    redirect_to '/login'
+    flash[:notice] = "You have successfully logged out."
+    redirect_to root_url
   end
   
 end
