1694a07f016b3b056d0abbdbc6409c8bb5259d62
lpenstone <laura.penstone@queensu.ca>
2017-08-12 15:11:38 +0000

    Generate controllers, views, and add routes for web pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
-  resources :users
+
+  get 'welcome/index'
+
+  root 'welcome#index'
+
+  resources :users, only: [:new, :create, :index, :show, :destroy]
+    get 'register', to: 'users#new'
+
+  namespace :admin do
+    root to: 'reviews#index'
+    resources :reviews, only: [:index, :destroy]
+    resources :users, only: [:index, :destroy]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
