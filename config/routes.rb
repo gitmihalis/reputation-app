0088b3a9ef6838e2fc737e6ce4d53d83dfb5a2a8
Mihalis Fthenos <mihalisfthenos@icloud.com>
2017-08-12 13:13:22 -0400

    update routes with sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   # these routes are for showing users a login form, logging them in, and logging them out.
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  
   resources :users
 end
