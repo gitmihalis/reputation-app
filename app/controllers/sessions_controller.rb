f3ddd7aa4db57ab4a3b824522f8095cb5709aea6
Mihalis Fthenos <mihalisfthenos@icloud.com>
2017-08-12 20:41:06 -0400

    fix session routes & controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,13 +10,13 @@ class SessionsController < ApplicationController
       log_in user
       redirect_to user
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
 
   def destroy
-    sessions[:user_id] = nil
+    session[:user_id] = nil
     flash[:notice] = "You have successfully logged out."
     redirect_to root_url
   end
