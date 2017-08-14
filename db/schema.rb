9b962660f5cab693ef0e52c48a02dd7c32140de9
Mihalis Fthenos <mihalisfthenos@icloud.com>
2017-08-14 14:56:38 -0400

    add remember_digest column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170812173647) do
+ActiveRecord::Schema.define(version: 20170814185548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20170812173647) do
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
     t.boolean  "admin",           default: false
+    t.string   "remember_digest"
   end
 
 end
