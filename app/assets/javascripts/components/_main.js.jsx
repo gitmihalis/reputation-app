a755a48067e3f7507fdfe26092a2e92f5abcbb42
lpenstone <laura.penstone@queensu.ca>
2017-08-16 01:33:22 +0000

    Further styling to user profiles

--- app/assets/javascripts/components/_main.js.jsx
+++ app/assets/javascripts/components/_main.js.jsx
@@ -2,8 +2,9 @@ class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      title: "All Reviews",
       reviews: this.props.reviews,
-      authors: this.props.authors
+      authors: this.props.authors,
     };
     this.addReview = this.addReview.bind(this);
     this.showNegReviews = this.showNegReviews.bind(this);
@@ -24,6 +25,7 @@ class Main extends React.Component {
 
   showNegReviews(){
     this.setState({
+      title: "Negative Reviews",
       reviews: this.props.reviewsneg,
       authors: this.props.authorsneg
     });
@@ -31,6 +33,7 @@ class Main extends React.Component {
 
   showAllReviews(){
     this.setState({
+      title: "All Reviews",
       reviews: this.props.reviews,
       authors: this.props.authors
     });
@@ -38,6 +41,7 @@ class Main extends React.Component {
 
   showWrittenReviews(){
     this.setState({
+      title: 'Reviews written by ' + this.props.receiver.first_name,
       reviews: this.props.reviewswritten,
       authors: this.props.received
     });
@@ -49,40 +53,70 @@ class Main extends React.Component {
 
 
   render() {
+    $(document).scroll(function() {
+      var y = $(document).scrollTop(),
+          header = $("#test");
+      if(y >= 550)  {
+          header.css({position: "fixed", "top" : "0", "left" : "0"});
+      } else {
+          header.css("position", "relative");
+      }
+    });
+
     let i = -1;
     const listReviews = this.state.reviews.map((review) => {
     i++;
     return (
-      <div key={review.id}>
-        <p>{this.state.authors[i].first_name} {this.state.authors[i].last_name}</p>
-        <p>{review.content}</p>
+      <div className = "review" key={review.id}>
+        <span className = "float-right">
+          <p>June 4th, 2017 </p>
+        </span>
+        <span className = "float-left">
+          <div className = "circle-frame" />
+        </span>
+          <span className = "reviewer-name">
+            <p>{this.state.authors[i].first_name} {this.state.authors[i].last_name}</p>
+          </span>
+          <span className = "reviewing-as">
+            <p>Hard coded, Reviewing {this.props.receiver.first_name} as Seller</p>
+          </span>
+        <div className = "content">
+          <p>{review.content}</p>
+        </div>
       </div>
     )
     });
     return (
       <div>
-        <ReviewBox
-          addReview = {this.addReview}
-          reviews = {this.state.reviews}
-          token = {this.props.token}
-          categories = {this.props.categories}
-          current_user = {this.props.current_user}
-          receiver = {this.props.receiver}/>
-
         <div className = "left-side-bar">
           <div className = "avatar">
             <img className = "resize-image" src = "https://content-static.upwork.com/uploads/2014/10/02123010/profile-photo_friendly.jpg" />
           </div>
-          <p onClick={event => { this.showNegReviews() } } ><strong> Show Negative Reviews </strong></p>
-          <p onClick={event => { this.showAllReviews() } } ><strong> Show All Reviews </strong></p>
-          <p onClick={event => { this.showWrittenReviews() } } ><strong> Show Written Reviews </strong></p>
+          <div className = "select-reviews" onClick={event => { this.showAllReviews() } }>
+            <p><strong>All Reviews </strong></p>
+          </div>
+          <div className = "select-reviews" onClick={event => { this.showWrittenReviews() } } >
+            <p><strong>Written Reviews </strong></p>
+          </div>
+          <div className = "select-reviews" onClick={event => { this.showNegReviews() } }>
+            <p className = "negative" ><strong>Negative Reviews </strong></p>
+          </div>
         </div>
         <div className = "right-box">
-          <h1> {this.props.receiver.first_name} {this.props.receiver.last_name}</h1>
+                <div id = "test">
+          <ReviewBox
+            addReview = {this.addReview}
+            reviews = {this.state.reviews}
+            token = {this.props.token}
+            categories = {this.props.categories}
+            current_user = {this.props.current_user}
+            receiver = {this.props.receiver}/>
+        </div>
+          <h1 className = "name"> {this.props.receiver.first_name} {this.props.receiver.last_name}</h1>
           <div className = "bio-box">
             <p> Hard coded Bio. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.  </p>
           </div>
-          <h1>Reviews:</h1>
+          <h1 className = "review-type-title" >{this.state.title}</h1>
           {listReviews}
         </div>
       </div>
