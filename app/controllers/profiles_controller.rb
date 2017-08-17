f585776286d28c8303db1153b299a5f42e93e93d
Mihalis Fthenos <mihalisfthenos@icloud.com>
2017-08-17 18:24:12 -0400

    form updates bio text

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,24 +5,17 @@ class ProfilesController < ApplicationController
 
   def update
     profile = Profile.find_by(id: params[:id])
-    if profile.update!(profile_params)
-      respond_to do |format|
-        format.html { redirect_to user_path(3), notice: 'Profile was successfully updated.' }
-        format.json { render json: profile, status: :created, location: profile }
-        format.js
-      end
+    user = profile.user
+    if profile.update(profile_params)
+      redirect_to user_path(user) , notice: 'Profile was successfully updated.'
     else
-      respond_to do |format|
-        format.html { redirect_to user_path(3), notice: 'Oh no! Profile was not updated.' }
-        format.json { render json: profile, status: :rejected, location: profile }
-        format.js
-      end
+      redirect_to user_path(user), notice: 'Oh no! Profile was not updated.' 
     end
   end
 
   private
 
   def profile_params
-    params.require(:profile).permit(:bio, :avatar)
+    params.fetch(:profile).permit(:bio, :avatar)
   end
 end
