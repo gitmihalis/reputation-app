e1f665396caa2dd32e588d5bd9edfe2046d8e538
egomatsushita <eduardo.go.rj@gmail.com>
2017-08-18 17:27:19 +0000

    Add conditions to validate when there is retracted or total reviews is 0

--- app/assets/javascripts/components/_scoreChart.js.jsx
+++ app/assets/javascripts/components/_scoreChart.js.jsx
@@ -1,5 +1,4 @@
 class ScoreChart extends React.Component {
-    // TESTING
   constructor(props) {
     super(props);
     this.state = {
@@ -8,11 +7,16 @@ class ScoreChart extends React.Component {
   }
 
   render() {
-    const angle = (100 - this.state.credScore) * 360 / 100;
-    const rotation = 270 - angle;
+    let angle = 0;
+    let rotation = 0;
     let divStyle = {};
+    let content = '';
 
+    // if positive reviews are above 50% chart background color is red and slices one and two colors are green
+    // otherwise, chart background color is green and slices one and two colors are red
     if (this.state.credScore >= 50) {
+      angle = (100 - this.state.credScore) * 360 / 100;
+      rotation = 270 - angle;
       divStyle = {
         chart2: {
           background: "#da0909",
@@ -26,6 +30,8 @@ class ScoreChart extends React.Component {
         }
       };
     } else {
+      angle = (this.state.credScore) * 360 / 100;
+      rotation = 270 - angle;
       divStyle = {
         chart2: {
           background: "#139838",
@@ -40,17 +46,38 @@ class ScoreChart extends React.Component {
       };
     }
 
+    // if credScore is 'No Reviews' print without percentage sign
+    if (this.state.credScore == 'No Reviews') {
+      content = `${this.state.credScore}`;
+      divStyle = {
+        chart2: {
+          background: "#989097",
+        },
+        sOne: {
+          background: "#989097",
+        },
+        sTwo: {
+          background: "#989097",
+        },
+        contentSpan: {
+          fontSize: '26px',
+          color: '#139838',
+        }
+      };
+    } else {
+      content = `${this.state.credScore} %`;
+    }
+
     return (
       <div className="card">
         <div className="donut-chart chart2" style={divStyle.chart2}>
           <div className="slice one" style={divStyle.sOne}></div>
           <div className="slice two" style={divStyle.sTwo}></div>
           <div className="chart-center" >
-            <span>{`${this.state.credScore} %`}</span>
+            <span style={divStyle.contentSpan}>{content}</span>
           </div>
         </div>
       </div>
     )
-    // TESTING
   }
 }
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,11 +57,19 @@ class UsersController < ApplicationController
 
     # Calculate credibilty score
     @total_of_reviews = @reviews.size
+
     @positive_reviews = 0
     @reviews.each do |review|
-      review.positive ? @positive_reviews += 1 : nil
+      @positive_reviews += 1 if review.positive  # increment positive reviews by one if review is positive
+      @total_of_reviews -= 1 if review.retracted  # decrement total of reviews by one if review is retracted
     end
-    @credibility_score = @positive_reviews * 100 / @total_of_reviews
+
+    if @total_of_reviews <= 0
+      @credibility_score = "No Reviews"
+    else
+      @credibility_score = @positive_reviews * 100 / @total_of_reviews
+    end
+
   end
 
   private # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
