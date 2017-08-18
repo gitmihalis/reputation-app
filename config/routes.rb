99f2d0936484190c1010435da9d1c8c20d4ddd01
egomatsushita <eduardo.go.rj@gmail.com>
2017-08-18 22:41:54 +0000

    Move search controller to users#index and delete everything related to search controller

--- config/routes.rb
+++ config/routes.rb
@@ -24,5 +24,4 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
-  get '/search', to: 'search#show'
 end
