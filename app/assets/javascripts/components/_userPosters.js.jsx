75067035c9c0cba5ac0586a281609ba6a3d1395c
lpenstone <laura.penstone@queensu.ca>
2017-08-22 00:06:55 +0000

    User data is mapped to profile data

--- app/assets/javascripts/components/_userPosters.js.jsx
+++ app/assets/javascripts/components/_userPosters.js.jsx
@@ -1,13 +1,24 @@
 class UserPosters extends React.Component {
   render() {
     let posters = [];
-    this.props.users.forEach((user) => {
-      if (user.first_name.toLowerCase().indexOf(this.props.filterText.toLowerCase()) === -1 &&
-          user.last_name.toLowerCase().indexOf(this.props.filterText.toLowerCase()) === -1) {
-        return;
+
+    for (var i = 1; i <= Object.keys(this.props.profile_details).length; i++ ){
+      if (!(this.props.profile_details[i]["user_first_name"].toLowerCase().indexOf(this.props.filterText.toLowerCase()) === -1 &&
+        this.props.profile_details[i]["user_last_name"].toLowerCase().indexOf(this.props.filterText.toLowerCase()) === -1)) {
+
+        posters.push(
+          <UserPoster
+          key= {i}
+          id= {this.props.profile_details[i]["user_id"]}
+          first_name = {this.props.profile_details[i]["user_first_name"]}
+          last_name = {this.props.profile_details[i]["user_last_name"]}
+          bio = {this.props.profile_details[i]["profile"][0]["bio"]}
+          rep_status = {this.props.profile_details[i]["profile"][0]["rep_status"]}
+          avatar = {this.props.profile_details[i]["profile"][0]["avatar"]["url"]}
+          />
+        );
       }
-      posters.push(<UserPoster user={user} key={user.id} profile={this.props.profiles[user.id - 1]} />);
-    });
+    }
     return (
       <div className = "outer-box">
         {posters}
@@ -15,3 +26,4 @@ class UserPosters extends React.Component {
     );
   }
 }
+
